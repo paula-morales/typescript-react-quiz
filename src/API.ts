ca6c44504dcbbcb7a6b6c97d923303c57a4d7b89
paula-morales <pmpoclava@gmail.com>
2020-07-15 19:25:46 +0200

    fetch questions

---
+++ src/API.ts
@@ -0,0 +1,15 @@
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+
+  //fisrt await fetch and then await json
+  const data = await (await fetch(endpoint)).json();
+  console.log("data endpoint", data);
+};
